4acd0b8b1a5084907d34199a0acd40320bc5007b
John Ritsema <john.ritsema@warnermedia.com>
2020-11-13 09:49:13 -0500

    handle sigterms

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+
+	//exit process immediately upon sigterm
+	handleSigTerms()
+
 	fmt.Println("hello")
+}
+
+func handleSigTerms() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("received SIGTERM, exiting")
+		os.Exit(1)
+	}()
 }
