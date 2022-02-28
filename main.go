a1ed3ec2bd6add7ebf75cd568a4f31b9de989380
John Ritsema <john_ritsema@yahoo.com>
2022-02-27 21:41:24 -0500

    adds run.go

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ func main() {
 	//exit process immediately upon sigterm
 	handleSigTerms()
 
-	fmt.Println("hello")
+	//run
+	if err := run(os.Args, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
 
 func handleSigTerms() {
